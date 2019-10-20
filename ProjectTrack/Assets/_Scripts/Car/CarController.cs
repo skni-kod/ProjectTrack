2fe6c1e9bca69bf9c4f2cfa55c6dc261da98361c
Paweł Dziedzic <blavikenn@gmail.com>
2019-10-20 15:00:38 +0200

    Add CarController script -> first prototype

---
+++ ProjectTrack/Assets/_Scripts/Car/CarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+    public Rigidbody sphereBody;
+
+    public float maxSpeed;
+
+    public float forceMultiplier;
+    public float turnSpeed;
+
+    Vector3 sphereOffset;
+
+    void Awake() {
+        sphereOffset = transform.position - sphereBody.position;
+        sphereBody.transform.parent = null;
+    }
+
+    void FixedUpdate() {
+        var horizontal = Input.GetAxis("Horizontal");
+        var verical = Input.GetAxis("Vertical");
+
+        float forceValue = Mathf.Clamp(forceMultiplier * verical, 0, maxSpeed);
+        var forceVector = transform.forward * forceValue;
+
+        sphereBody.AddForce(forceVector);
+        transform.Rotate(0, horizontal * turnSpeed, 0);
+
+        transform.position = sphereBody.position + sphereOffset;
+    }
+}
