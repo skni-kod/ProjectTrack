2d076eb3a3e3431fff4031cf124470801631e6e9
Paweł Dziedzic <blavikenn@gmail.com>
2019-10-22 14:39:40 +0200

    Add new input system, camera controller, car rotation

--- ProjectTrack/Assets/_Scripts/Car/CarController.cs
+++ ProjectTrack/Assets/_Scripts/Car/CarController.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour {
+
+    public LayerMask trackMask;
+    public float rayLength;
     public Rigidbody sphereBody;
 
     public float maxSpeed;
@@ -12,21 +16,42 @@ public class CarController : MonoBehaviour {
 
     Vector3 sphereOffset;
 
+    Vector2 inputVector;
+
     void Awake() {
         sphereOffset = transform.position - sphereBody.position;
         sphereBody.transform.parent = null;
     }
 
+    void OnMove(InputValue input) {
+        inputVector = input.Get<Vector2>();
+    }
+
     void FixedUpdate() {
-        var horizontal = Input.GetAxis("Horizontal");
-        var verical = Input.GetAxis("Vertical");
-
-        float forceValue = Mathf.Clamp(forceMultiplier * verical, 0, maxSpeed);
-        var forceVector = transform.forward * forceValue;
-
-        sphereBody.AddForce(forceVector);
-        transform.Rotate(0, horizontal * turnSpeed, 0);
-
         transform.position = sphereBody.position + sphereOffset;
+
+        bool grounded = Physics.Raycast(transform.position, Vector2.down, out RaycastHit hit, rayLength, trackMask);
+        if (grounded) {
+            var groundNormal = hit.normal;
+            var forwardVector = Vector3.Cross(transform.right, groundNormal);
+
+            transform.rotation = Quaternion.LookRotation(forwardVector, groundNormal);
+
+            var forceVector = transform.forward * forceMultiplier * inputVector.y;
+
+            sphereBody.AddForce(forceVector);
+            sphereBody.velocity = Vector3.ClampMagnitude(sphereBody.velocity, maxSpeed);
+
+            transform.Rotate(transform.up * inputVector.x * turnSpeed);
+        }
+    }
+
+    void OnGUI() {
+        GUILayout.Label(sphereBody.velocity.magnitude.ToString());
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Vector3.down * rayLength);
     }
 }
