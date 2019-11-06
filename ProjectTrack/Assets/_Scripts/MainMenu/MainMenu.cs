6a5be35b20d2b54eba29901bcecd4c232aedf988
AteX01 <atex1999@gmail.com>
2019-11-06 22:18:16 +0100

    Add simple main menu with PLAY button

---
+++ ProjectTrack/Assets/_Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button playButton;
+ 
+    public string gameScene;
+
+    void Start()
+    {
+        Button btn = playButton.GetComponent<Button>();
+        btn.onClick.AddListener(startGame);
+    }
+
+    void startGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+}
