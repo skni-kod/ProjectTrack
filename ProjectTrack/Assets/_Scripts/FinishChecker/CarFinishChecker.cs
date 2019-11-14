f108f3c993ca2df99f0121c23324aac51c6e30fb
Bernes <56940210+Bernes99@users.noreply.github.com>
2019-11-14 23:25:14 +0100

    Added simple finish checker

---
+++ ProjectTrack/Assets/_Scripts/FinishChecker/CarFinishChecker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarFinishChecker : MonoBehaviour
+{
+    private bool orderChecker=true;
+    private bool nie = false;
+    private int i=1;
+    public CheckPointChecker CheckPoint;
+
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.name=="backDetector" && orderChecker == true  )
+        {
+            orderChecker = false;
+            if (i == 1)
+            {
+
+                nie = true;
+            }
+            else if (i == 0)
+            {
+                nie = false;
+            }
+
+        }
+        else if (coll.gameObject.name == "backDetector" && orderChecker == false )
+        {
+            if (nie == false)
+            {
+                orderChecker = true;
+                nie = false;
+            }
+            else
+                i--;
+
+        }
+        if (coll.gameObject.name == "frontDetector" && orderChecker == true)
+        {
+            if(CheckPoint.checkPoint ==true)
+            {
+                Debug.Log("Punkt");
+                CheckPoint.checkPoint = false;
+            } 
+            nie = false;
+            orderChecker = false;
+        }
+        else if (coll.gameObject.name == "frontDetector" && orderChecker ==false)
+        {
+            orderChecker = true;
+            
+        }
+        
+        
+        
+
+    }
+    
+    
+
+}
---
+++ ProjectTrack/Assets/_Scripts/FinishChecker/CheckPointChecker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointChecker : MonoBehaviour
+{
+    public bool checkPoint=false;
+    public void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.name == "frontDetector")
+        {
+            checkPoint = true;
+            
+        }
+
+    }
+}
