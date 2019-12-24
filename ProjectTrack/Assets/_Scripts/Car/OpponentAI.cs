1373d83a0d9e970d2572f81fbe1e6fb6ecb34c83
Rafał Nazarko <rafal.nazarko@gmail.com>
2019-12-24 14:51:21 +0100

    add simple AI system

---
+++ ProjectTrack/Assets/_Scripts/Car/OpponentAI.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpponentAI : MonoBehaviour
+{
+    public Transform path;
+    public Rigidbody sphereBody;
+    private int currentNode = 1;
+    private List<Transform> nodes = null;
+    public float maxSpeed;
+    public float maxForce;
+    Vector3 sphereOffset;
+    public float turnSpeed;
+    public float smooth;
+
+    void Start()
+    {
+        sphereOffset = transform.position - sphereBody.position;
+        sphereBody.transform.parent = null;
+
+    Transform[] pathTransform = path.GetComponentsInChildren<Transform>();
+      nodes = new List<Transform>();
+
+      for (int i = 0; i < pathTransform.Length; i++)
+      {
+          if(pathTransform[i] != transform)
+          {
+            nodes.Add(pathTransform[i]);
+          }
+      }
+    }
+
+    void FixedUpdate()
+    {
+        ApplySteer();
+        DistanceFromWaypoint();
+    }
+
+    Vector3 relativeVector;
+
+    private void ApplySteer(){
+        bool grounded = Physics.Raycast(transform.position, Vector2.down, out RaycastHit hit);
+        relativeVector = sphereBody.transform.InverseTransformPoint(nodes[currentNode].position);
+        sphereBody.AddRelativeForce((relativeVector/CalculateDistane(transform.position,nodes[currentNode].position)) * maxForce);
+        sphereBody.velocity = Vector3.ClampMagnitude(sphereBody.velocity, maxSpeed);
+        transform.position = sphereBody.position + sphereOffset;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(sphereBody.velocity, Vector3.up), (smooth * Time.deltaTime));
+    }
+
+    private void OnDrawGizmos() {
+        if(currentNode > 1)
+        {
+            Gizmos.DrawLine(sphereBody.position,nodes[currentNode].position);
+        }
+    }
+    
+    private float CalculateDistane(Vector3 first, Vector3 second)
+    {
+        return Vector3.Distance(first,second);
+    }
+
+
+    private void DistanceFromWaypoint() {
+        if(this.CalculateDistane(transform.position,nodes[currentNode].position) < 2f)
+        {
+            if(currentNode == nodes.Count-1)
+            {
+                currentNode = 1;
+            }
+            else
+            {
+                currentNode += 1;
+            }
+        }
+    }
+}
---
+++ ProjectTrack/Assets/_Scripts/Path.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+  public Color pathColor;
+
+  private List<Transform> nodes = new List<Transform>();
+
+  void OnDrawGizmos()
+  {
+      Gizmos.color = pathColor;
+
+      Transform[] pathTransform = GetComponentsInChildren<Transform>();
+      nodes = new List<Transform>();
+
+      for (int i = 0; i < pathTransform.Length; i++)
+      {
+          if(pathTransform[i] != transform)
+          {
+              nodes.Add(pathTransform[i]);
+          }
+      }
+
+      for (int i = 0; i < nodes.Count; i++)
+      {
+        Vector3 currentNode = nodes[i].position;  
+        Vector3 previousNode = Vector3.zero;
+
+        if(i == 0 && nodes.Count > 1)
+        {
+            previousNode = nodes[nodes.Count-1].position;
+        }
+        else{
+            previousNode = nodes[i-1].position;
+        }
+
+        Gizmos.DrawLine(previousNode, currentNode);
+        Gizmos.DrawWireSphere(currentNode, 0.3f);
+
+      }
+  }
+}
